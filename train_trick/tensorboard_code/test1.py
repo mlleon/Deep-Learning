19d182bb57ffc9d0bd20ae3ab7572e2c4a0d4b3d
leon <877313022@qq.com>
2023-12-18 06:51:33 +0800

    add files

--- train_trick/tensorboard_code/test1.py
+++
@@ -1,7 +0,0 @@
-from torch.utils.tensorboard import SummaryWriter
-import numpy as np
-log_dir="./train_log/test_log_dir"
-writer = SummaryWriter(log_dir=log_dir)
-for x in range(1, 101) :
-    writer.add_scalar('y = 2x', x, 2 * x)
-writer.close()
